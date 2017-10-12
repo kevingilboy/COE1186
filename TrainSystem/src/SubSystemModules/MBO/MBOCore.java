1df39b6736476dcb3a064d362613e1972268a5aa
dylan <dylan.mcguire@pitt.edu>
2017-10-12 15:11:48 -0400

    added functionality to read train info from MBOCore

--- TrainSystem/src/SubSystemModules/MBO/MBOCore.java
+++ TrainSystem/src/SubSystemModules/MBO/MBOCore.java
@@ -1,10 +1,46 @@
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.time.format.*;
 
 public class MBOCore {
 
 	private TrainInfo[] trains;
 	private TrackInfo track;
+
+	public MBOCore(String configFile) {
+		System.out.println("MBO loading configuration from " + configFile);
+		demoInit();
+	}
+
+	private void demoInit() {
+		trains = new TrainInfo[5];
+		trains[0] = new TrainInfo("RED 1", new double[] {5,5}, LocalDateTime.now());
+		trains[1] = new TrainInfo("RED 2", new double[] {5,-5}, LocalDateTime.now());
+		trains[2] = new TrainInfo("GREEN 1", new double[] {-5,5}, LocalDateTime.now());
+		trains[3] = new TrainInfo("GREEN 2", new double[] {-5,-5}, LocalDateTime.now());
+		trains[4] = new TrainInfo("GREEN 3", new double[] {0,0}, LocalDateTime.now());
+	}
+
+	public Object[][] getTrainData() {
+		updateInfo();
+		Object[][] results = new Object[trains.length][6];
+		for (int i = 0; i < trains.length; i++) {
+			results[i] = new Object[] {trains[i].name, 
+				                       trains[i].signalReceived.format(DateTimeFormatter.ofPattern("HH:mm:ss")), 
+				                       Arrays.toString(trains[i].position),
+				                       null,
+				                       trains[i].speed,
+				                       trains[i].currentAuthority};
+		}
+		return results;
+	}
+
+	public void updateInfo() {
+		for (TrainInfo train : trains) {
+			train.updateLatestSignal(train.position, LocalDateTime.now());
+		}
+	}
 
 	private static class TrainInfo {
 
@@ -30,9 +66,7 @@ public class MBOCore {
 		}
 
 		private void calculateVelocity() {
-			System.out.println(previousPosition[0] + "." + previousPosition[1]);
 			double elapsedHours = previousSignalReceived.until(signalReceived, ChronoUnit.MILLIS);
-			System.out.println(elapsedHours);
 			velocity[0] = (position[0] - previousPosition[0]) / elapsedHours;
 			velocity[1] = (position[1] - previousPosition[1]) / elapsedHours;
 			speed = Math.pow(Math.pow(velocity[0], 2) + Math.pow(velocity[1], 2), 0.5);
@@ -55,24 +89,5 @@ public class MBOCore {
 
 	private static class TrackInfo {
 
-	}
-
-	public static void main(String[] args) {
-		TrainInfo train = new TrainInfo("RED 1", new double[] {0,0}, LocalDateTime.now());
-		for (Object field : train.getFields()) {
-			System.out.println(field);
-		}
-		double[] pos1 = {1,1};
-		double[] pos2 = {2,2};
-		train.updateLatestSignal(pos1, LocalDateTime.now());
-		for (Object field : train.getFields()) {
-			System.out.println(field);
-		}
-		train.updateLatestSignal(pos2, LocalDateTime.now());
-		for (Object field : train.getFields()) {
-			System.out.println(field);
-		}
-
-		
 	}
 }
