1812961019c64023ab5262bcfa970b7579fc3268
Kevin Gilboy <kmg130@pitt.edu>
2017-11-13 20:32:39 -0500

    Addition of train model to Simulator

--- TrainSystem/src/Simulator/Simulator.java
+++ TrainSystem/src/Simulator/Simulator.java
@@ -64,6 +64,7 @@ public class Simulator {
 		trainController.trainModel = trainModel;
 
 		mbo.trainController = trainController;
+		mbo.trainModel = trainModel;
 		
 		temperature = 69;
 		weather = "Sunny";
