8e42f2f50f45f6eb8dfc548ca9254f72137e38e1
Kevin Gilboy <kmg130@pitt.edu>
2017-11-13 20:36:39 -0500

    Addition of train model reference in Mbo

--- TrainSystem/src/Modules/Mbo/Mbo.java
+++ TrainSystem/src/Modules/Mbo/Mbo.java
@@ -4,9 +4,11 @@ import Shared.Module;
 import Shared.SimTime;
 
 import Modules.TrainController.TrainController;
+import Modules.TrainModel.TrainModel;
 
 public class Mbo implements Module{
 	public TrainController trainController;
+	public TrainModel trainModel;
 
 	public Mbo(){
 
