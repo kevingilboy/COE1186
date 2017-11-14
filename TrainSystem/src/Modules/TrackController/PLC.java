3dd8144e9c848c27d14e5930399774b382df8c09
Nick Petro <nickpetro@pitt.edu>
2017-11-14 16:03:38 -0500

    Added code from archive for uploading and parsing PLC. Need to update for unique syntax

--- TrainSystem/src/Modules/TrackController/PLC.java
+++ TrainSystem/src/Modules/TrackController/PLC.java
@@ -9,7 +9,46 @@ public class PLC{
 	}
 	
 	public boolean parsePLC(String plcPath){
-		
+		System.out.println("parsing file: " + plcPath);
+		/*BufferedReader 	br 			= null;
+		String 			currline 	= "";
+		String 			delimeter 	= ":";
+
+		try {
+			br = new BufferedReader(new FileReader(path));
+			String [] plcString = new String[8];
+			int location = 0;
+			
+			// Read from plc file and create logic for each line, then
+			// add each statement to an arraylist of logic statements
+
+			while ((currline = br.readLine()) != null){
+				String [] logicStatement = currline.split(delimeter);
+				location++;
+				
+				String 	logicFor 		= logicStatement[0].replaceAll("\\s+","");
+				String 	logic 			= logicStatement[1].replaceAll("\\s+","");
+				
+				plcString[location-1] = logicFor;
+				plcString[location] = logic;
+					
+				System.out.println(logicFor);
+				System.out.println(logic);
+			}
+			newPlc = new Plc(plcString[0], plcString[1], plcString[2], plcString[3], plcString[4], plcString[5], plcString[6], plcString[7]);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }*/
 		return true;
 	}
 	
--- TrainSystem/src/Modules/TrackController/TrackControllerGUI.java
+++ TrainSystem/src/Modules/TrackController/TrackControllerGUI.java
@@ -1,20 +1,37 @@
 public class TrackControllerGUI{
+	//Parent Class
+	private TrackController tc;
 	//Gui Variables
 	private String line;
 	private String section;
 	private int blockId;
 	private String plcPath;
 	
-	public TrackControllerGUI(){
-		
+	public TrackControllerGUI(TrackController tc){
+		this.tc = tc;
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TrackControllerGUI frame = new TrackControllerGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 	
 	private void displayInfo(){
 		
 	}
-	
-	private void uploadPLC(){
-		
+
+	class uploadPLC implements ActionListener {
+	    public void actionPerformed(ActionEvent e) {
+			JFileChooser c = new JFileChooser();
+			String plcPath = c.getCurrentDirectory().toString() + "/" + c.getSelectedFile().getName();
+			System.out.println("path = " + plcPath;
+			boolean parserSuccess = tc.tcplc.parsePlc(plcPath);
+	    }
 	}
 	
 }
