ea5ca6d9f31154b5d37550c9e69e0065e43e6bbe
Nick Petro <nickpetro@pitt.edu>
2017-11-14 13:11:45 -0500

    Initial skeletons of subclasses

---
+++ TrainSystem/src/Modules/TrackController/PLC.java
@@ -0,0 +1,27 @@
+public class PLC{
+	//PLC Variables
+	private String lightLogic;
+	private String switchLogic;
+	private String crossingLogic;
+	
+	public PLC(){
+		
+	}
+	
+	public boolean parsePLC(String plcPath){
+		
+		return true;
+	}
+	
+	public String getLightLogic(){
+		return lightLogic;
+	}
+	
+	public String getSwitchLogic(){
+		return switchLogic;
+	}
+	
+	public String getCrossingLogic(){
+		return crossingLogic;
+	}
+}
---
+++ TrainSystem/src/Modules/TrackController/TrackControllerGUI.java
@@ -0,0 +1,20 @@
+public class TrackControllerGUI{
+	//Gui Variables
+	private String line;
+	private String section;
+	private int blockId;
+	private String plcPath;
+	
+	public TrackControllerGUI(){
+		
+	}
+	
+	private void displayInfo(){
+		
+	}
+	
+	private void uploadPLC(){
+		
+	}
+	
+}
