627cf4421d510557fce2cfddbd92ca7136ac3d24
Kevin Gilboy <kmg130@pitt.edu>
2017-11-14 01:36:07 -0500

    Addition of initial location to Train class

--- TrainSystem/src/Modules/Ctc/Train.java
+++ TrainSystem/src/Modules/Ctc/Train.java
@@ -1,9 +1,11 @@
 package Modules.Ctc;
 
+import Modules.TrackModel.Block;
+
 public class Train {
 	public String name;
 	public Line line;
-	public int location;
+	public Block location;
 	public int speed;
 	public int authority;
 	public Schedule schedule;
@@ -12,6 +14,14 @@ public class Train {
 	public Train(Schedule schedule) {
 		this.schedule = schedule;
 		this.line = schedule.line;
+		this.passengers = 0;
+		
+		if(line==Line.RED) {
+			location = line.blocks[77];
+		}
+		else if(line == Line.GREEN) {
+			location = line.blocks[152];
+		}
 	}
 
 }
