a95aa8b492eb36cad5018835665a0fd27975ab2d
Nick Petro <nickpetro@pitt.edu>
2017-11-13 20:06:16 -0500

    Built out the basic structure for the main class

--- TrainSystem/src/Modules/TrackController/TrackController.java
+++ TrainSystem/src/Modules/TrackController/TrackController.java
@@ -6,8 +6,24 @@ import Shared.SimTime;
 import Modules.TrackModel.TrackModel;
 
 public class TrackController implements Module{
+	//Set by other modules
 	public TrackModel trackModel;
+	public String associatedLine;
+	public int[] associatedBlocks;
+	//Set internally
+	private String line;
+	private String section;
+	private int blockId;
+	private boolean hasLight;
+	private boolean lightState;
+	private boolean hasSwitch;
+	private boolean switchState;
+	private boolean hasCrossing;
+	private boolean crossingState;
+	private boolean status;
+	private boolean occupancy;
 
+	//Constructor
 	public TrackController(){
 
 	}
@@ -16,5 +32,65 @@ public class TrackController implements Module{
 	public boolean updateTime(SimTime time) {
 
 		return true;
+	}
+	//Internal Functions
+	private void receiveBlockInfo(String line, int blockId){
+		hasLight = trackModel.getBlock(line, blockId);
+		lightState = trackModel.getBlock(line, blockId);
+		hasSwitch = trackModel.getBlock(line, blockId);
+		switchState = trackModel.getBlock(line, blockId);
+		hasCrossing = trackModel.getBlock(line, blockId);
+		crossingState = trackModel.getBlock(line, blockId);
+		status = trackModel.getBlock(line, blockId);
+		occupancy = trackModel.getBlock(line, blockId);
+	}
+	
+	private void updateStates(){
+		
+	}
+	
+	private boolean uploadPLC(String plcPath){
+		return true;
+	}
+	
+	private void runPLC(boolean occupancy){
+		
+	}
+	
+	private void guiUpdate(){
+		
+	}
+	
+	//CTC Functions
+	public void transmitSuggestedTrainSetpointSpeed(String trainName, int speed){
+		trackModel.transmitSuggestedTrainSetpointSpeed(trainName, speed);
+	}
+	
+	public void transmitCtcAuthority(String trainName, int authority){
+		trackModel.transmitCtcAuthority(trainName, authority);
+	}
+	
+	public Block receiveBlockInfoForCtc(String line, int blockId){
+		return trackModel.getBlock(line, blockId);
+	}
+	
+	public void transmitBlockMaintenance(String line, int blockId, boolean maintenance){
+		trackModel.getBlock(line, blockId).setMaintenance(maintenance);
+	}
+	
+	//Hardware Control
+	public void transmitSwitchState(String line, int blockId, boolean state){
+		//logic before transmitting
+		trackModel.getBlock(line, blockId).getSwitch().setState(state);
+	}
+	
+	private void transmitLightState(String line, int blockId, boolean state){
+		//logic before transmitting
+		trackModel.getBlock(line, blockId).getLight().setState(state);
+	}
+	
+	private void transmitCrossingState(String line, int blockId, boolean state){
+		//logic before transmitting
+		trackModel.getBlock(line, blockId).getCrossing().setState(state);
 	}
 }
