eb77947b1e7b4986c4e9bf78da01298bab3ad2a1
Jennifer Patterson <jlp202@pitt.edu>
2017-11-15 21:42:44 -0500

    Select Train List is Populating
    - Support for dispatch added.
    - Train List items are populated in the Select Train list
    - Fixed some things with methods in the Train Model class, Train class and TrainModelGUI class to allow for less redundant code, more readable code and methods for easier data access.
    - Still need to

--- TrainSystem/src/Modules/TrainModel/TrainModel.java
+++ TrainSystem/src/Modules/TrainModel/TrainModel.java
@@ -18,14 +18,15 @@ import Modules.TrackModel.*;
  */
 public class TrainModel implements Module{
 	public TrackModel trackModel;
-	public static HashMap<String, Train> trainList;
-	Train train = new Train("GREEN", "Train 1", this);
+	public static HashMap<Integer, Train> trainList;
 	public SimTime currentTime = new SimTime("00:00:00");
 	//Instantiate a GUI for this train
-    TrainModelGUI trainModelGUI = train.CreateNewGUI();
 
 	public TrainModel(){
-		trainList = new HashMap<String, Train>();
+		trainList = new HashMap<Integer, Train>();
+		Train train = new Train("GREEN", "Train 1", this);
+		dispatchTrain(train.getTrainID(), train);
+		//instantiateGUI(train);
         train.showTrainGUI();
 	}
 
@@ -35,17 +36,36 @@ public class TrainModel implements Module{
 	@Override
 	public boolean updateTime(SimTime time) {
 		currentTime = time;
-		train.updateVelocity();
-        train.setValuesForDisplay(trainModelGUI);
-        if(trainModelGUI.isDisplayable() == false) {
+		for(Train t : trainList.values()) {
+			t.updateVelocity();
+	        t.setValuesForDisplay();
+		}
+        //if(trainModelGUI.isDisplayable() == false) {
             //System.exit(0);
-        }
+        //}
 		return true;
 	}
 	
-	//public void addTrain(String trainID, Train train) {
-		
-	//}
+	/**
+	 * Dispatch a train to a specific block (will add that functionality later)
+	 * @param trainID
+	 * @param train
+	 */
+	public void dispatchTrain(String trainID, Train train) {
+		trainList.put(trainID.hashCode(), train);
+		instantiateGUI(train);
+	}
+	
+	private void instantiateGUI(Train train) {
+		TrainModelGUI trainModelGUI = train.CreateNewGUI();
+		trainModelGUI.addTraintoGUIList(train);
+		//return trainModelGUI;
+	}
+	
+	public Train getTrain(String ID) {
+		return trainList.get(ID.hashCode());
+	}
+	
 	
 	/*public static void runTrainModel () throws InterruptedException {
         Train train = new Train("GREEN", "Train 1");
@@ -67,16 +87,12 @@ public class TrainModel implements Module{
     }*/
 
 	
-	private void updateList() {
-		
-	}
-	
 	/*private int calcDeltaTime(SimTime start, SimTime end) {
 		return start-end; // TODO: Ask Kevin how we would be doing this
 	}*/
 	
 	/*public Train getTrainAtBlock(Block block) {
-		// TODO: Should I iterate through every single entry in the hasmap to find the
+		// TODO: Should I iterate through every single entry in the hashmap to find the
 		// train at the specified block?
 		return;
 	}*/
@@ -85,9 +101,10 @@ public class TrainModel implements Module{
 		
 	}
 	
-	public void dispatchTrainToBlock(/* Not sure what goes in here yet...*/) {
-		
+	public void dispatchTrainToBlock(String trainID, Train train, Block block) {
+		trainList.put(trainID.hashCode(), train);
 	}
+	
 	
 	public double transmitCtcAuthority(String trainID, double authority) {
 		return authority;
@@ -97,10 +114,35 @@ public class TrainModel implements Module{
 		return setpoint;
 	}
 	
-	public void setEmergencyBrake(String trainID, boolean eBrake) {
+	/**
+	 * Set the Emergency Brake status based on the train controller, so he will
+	 * call this setter if he sets the emergency brake. Then the passenger button
+	 * on the train itself will not be "pushable".
+	 * 
+	 * @param trainID
+	 * @param eBrake
+	 */
+	public void setDriverEmergencyBrake(String trainID, boolean eBrake) {
 		
 	}
 	
+	/**
+	 * Set the Emergency Brake status based on the passengers. In this case, the driver
+	 * will be allowed to override a request for the emergency brake, but only once the 
+	 * train has come to a complete stop can he deactivate the e brake
+	 * @param trainID
+	 * @param eBrake
+	 */
+	public void setPassengerEmergencyBrake(String trainID, boolean eBrake) {
+		
+	}
+	
+	/**
+	 * Set by the driver only
+	 * 
+	 * @param trainID
+	 * @param sBrake
+	 */
 	public void setServiceBrake(String trainID, boolean sBrake) {
 		
 	}
@@ -109,7 +151,7 @@ public class TrainModel implements Module{
 		return ;
 	}*/
 	
-	public void setPosition(String trainID, String[] coordinates) {
+	public void setPosition(String trainID, double[] coordinates) {
 		
 	}
 	
