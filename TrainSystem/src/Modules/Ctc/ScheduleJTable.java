1a8c521dbb72f3f8fb30b463b7dd723002e04086
Kevin Gilboy <kmg130@pitt.edu>
2017-11-18 00:56:56 -0500

    Removed unused imports

--- TrainSystem/src/Modules/Ctc/ScheduleJTable.java
+++ TrainSystem/src/Modules/Ctc/ScheduleJTable.java
@@ -1,7 +1,5 @@
 package Modules.Ctc;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
@@ -10,7 +8,6 @@ import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellEditor;
 
 import Modules.TrackModel.Block;
 
