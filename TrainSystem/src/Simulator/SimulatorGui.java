95bbfb32c626f633d60835984d45f3ebef8e7710
Kevin Gilboy <kmg130@pitt.edu>
2017-12-09 14:05:09 -0500

    Addition of MVP GUI components and event listeners

--- TrainSystem/src/Simulator/SimulatorGui.java
+++ TrainSystem/src/Simulator/SimulatorGui.java
@@ -3,9 +3,15 @@ package Simulator;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class SimulatorGui {
-
+	private enum Waysides{
+		R1,R2,G1,G2;
+	};
 	private JFrame frame;
 
 	/**
@@ -37,8 +43,131 @@ public class SimulatorGui {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 800, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		frame.getContentPane().setLayout(null);
+		
+		/*
+		 * ------------------------------
+		 *  CTC
+		 * ------------------------------
+		 */
+		JButton btnCtc = new JButton("CTC");
+		btnCtc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCtc.setBounds(26, 52, 171, 41);
+		frame.getContentPane().add(btnCtc);
+		
+		/*
+		 * ------------------------------
+		 *  TRACK CONTROLLER
+		 * ------------------------------
+		 */
+		JButton btnWaysideR1 = new JButton("R1");
+		btnWaysideR1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openGui(ModuleType.TRACKCONTROLLER,Waysides.R1);
+			}
+		});
+		btnWaysideR1.setBounds(26, 121, 65, 41);
+		frame.getContentPane().add(btnWaysideR1);
+		
+		JButton btnWaysideR2 = new JButton("R2");
+		btnWaysideR2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openGui(ModuleType.TRACKCONTROLLER,Waysides.R2);
+			}
+		});
+		btnWaysideR2.setBounds(101, 121, 65, 41);
+		frame.getContentPane().add(btnWaysideR2);
+		
+		JButton btnWaysideG1 = new JButton("G1");
+		btnWaysideG1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openGui(ModuleType.TRACKCONTROLLER,Waysides.G1);
+			}
+		});
+		btnWaysideG1.setBounds(188, 121, 65, 41);
+		frame.getContentPane().add(btnWaysideG1);
+		
+		JButton btnWaysideG2 = new JButton("G2");
+		btnWaysideG2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openGui(ModuleType.TRACKCONTROLLER,Waysides.G2);
+			}
+		});
+		btnWaysideG2.setBounds(275, 121, 65, 41);
+		frame.getContentPane().add(btnWaysideG2);
+		
+		/*
+		 * ------------------------------
+		 *  TRACK MODEL
+		 * ------------------------------
+		 */
+		JButton btnTrackModel = new JButton("Track Model");
+		btnTrackModel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openGui(ModuleType.TRACKMODEL);
+			}
+		});
+		btnTrackModel.setBounds(26, 186, 171, 41);
+		frame.getContentPane().add(btnTrackModel);
+		
+		/*
+		 * ------------------------------
+		 *  TRAIN MODEL
+		 * ------------------------------
+		 */
+		JComboBox<String> cbTrainModelTrains = new JComboBox<String>();
+		cbTrainModelTrains.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String trainName = (String)cbTrainModelTrains.getSelectedItem();
+				openGui(ModuleType.TRAINMODEL,trainName);
+			}
+		});
+		cbTrainModelTrains.setBounds(26, 255, 52, 39);
+		frame.getContentPane().add(cbTrainModelTrains);
+		
+		/*
+		 * ------------------------------
+		 *  TRAIN CONTROLLER
+		 * ------------------------------
+		 */
+		JButton btnTraincontroller = new JButton("TrainController");
+		btnTraincontroller.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openGui(ModuleType.TRAINCONTROLLER);
+			}
+		});
+		btnTraincontroller.setBounds(26, 332, 171, 41);
+		frame.getContentPane().add(btnTraincontroller);
+		
+		JComboBox<String> cbTrainControllerTrains = new JComboBox<String>();
+		cbTrainControllerTrains.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String trainName = (String)cbTrainControllerTrains.getSelectedItem();
+				openGui(ModuleType.TRAINCONTROLLER,trainName);
+			}
+		});
+		cbTrainControllerTrains.setBounds(223, 333, 52, 39);
+		frame.getContentPane().add(cbTrainControllerTrains);
+		
+		/*
+		 * ------------------------------
+		 *  MBO
+		 * ------------------------------
+		 */
+		JButton btnMbo = new JButton("MBO");
+		btnMbo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				openGui(ModuleType.MBO);
+			}
+		});
+		btnMbo.setBounds(26, 391, 171, 41);
+		frame.getContentPane().add(btnMbo);
+		
 	}
 
 	protected void moduleObjectInitialized(ModuleType module) {
@@ -101,9 +230,6 @@ public class SimulatorGui {
 			case TRACKMODEL:
 				//...
 				break;
-			case TRAINMODEL:
-				//...
-				break;
 			case TRAINCONTROLLER:
 				//...
 				break;
@@ -112,5 +238,23 @@ public class SimulatorGui {
 				break;		
 		}
 	}
-
+	private void openGui(ModuleType module, Waysides wayside) {
+		//We can use this to open a respective Module's GUI
+		switch (module) {
+			case TRACKCONTROLLER:
+				//...
+				break;
+		}
+	}
+	private void openGui(ModuleType module, String trainName) {
+		//We can use this to open a respective Module's GUI
+		switch (module) {
+			case TRAINMODEL:
+				//...
+				break;
+			case TRAINCONTROLLER:
+				//...
+				break;	
+		}
+	}
 }
