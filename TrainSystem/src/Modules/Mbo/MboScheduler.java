228f42fda3fc75ce78702b61c8de6bd601f3d66e
dylanscode <dylan.mcguire@pitt.edu>
2017-12-11 08:16:22 -0500

    Functionality to Read Schedules

--- TrainSystem/src/Modules/Mbo/MboScheduler.java
+++ TrainSystem/src/Modules/Mbo/MboScheduler.java
@@ -6,29 +6,56 @@ public class MboScheduler {
 	
 	private String date;
 	private int desiredThroughput;
+	private ArrayList<OperatorSchedule> operators;
+	private ArrayList<TrainSchedule> train;
 
 	private class OperatorSchedule {
 		private String name;
 		private LocalDateTime startTime;
 		private LocalDateTime stopTime;
+
+		private OperatorSchedule(String[] schedule) {
+			name = schedule[0];
+			startTime = schedule[1];
+			stopTime = schedule[2];
+		}
 	}
 
 	private class TrainSchedule {
 		private String name;
 		private LocalDateTime startTime;
 		private LocalDateTime stopTime;
+
+		private TrainSchedule(String[] schedule) {
+			name = schedule[0];
+			startTime = schedule[1];
+			stopTime = schedule[2];
+		}
 	}
 
 	public MboScheduler() {
-
+		this.operators = new ArrayList<OperatorSchedule>();
+		this.trains = new ArrayList<TrainSchedule>();
 	}
 
 	public boolean updateTrainSchedules(String[][] schedules) {
-		return false;
+		for (String[] schedule : schedules) {
+			if (schedule[0].equals("") && schedule[1].equals("") && schedule[2].equals("")) {
+				return false;
+			}
+			trains.add(new TrainSchedule(schedule));
+		}
+		return true;
 	}
 
 	public boolean updateOperatorSchedules(String[][] schedules) {
-		return false;
+		for (String[] schedule : schedules) {
+			if (schedule[0].equals("") && schedule[1].equals("") && schedule[2].equals("")) {
+				return false;
+			}
+			trains.add(new TrainSchedule(schedule));
+		}
+		return true;
 	}
 
 	public String generateSchedule(String filename, double throughput) {
